5ea6695a51033a732d5a79bfb1b04d10a5c43c1b
retroluxfilm <retroluxfilm@SILENTREAKTOR>
2022-03-01 17:40:01 +0100

    Fixed url encoded path to correct slashes and entry path point

--- src/Repository/RepositoryHelper.php
+++ src/Repository/RepositoryHelper.php
@@ -57,7 +57,7 @@ class RepositoryHelper
         // first url raw encode elements between directory seperator
         $urlEncoded = implode(DIRECTORY_SEPARATOR, array_map("rawurlencode", explode(DIRECTORY_SEPARATOR, $path)));
         // remove first relative path point and seperator
-        $urlEncoded = str_replace("." + DIRECTORY_SEPARATOR,"",$urlEncoded);
+        $urlEncoded = str_replace("." . DIRECTORY_SEPARATOR,"",$urlEncoded);
         // convert directory seperator to forward slashes
         $urlEncoded = str_replace(DIRECTORY_SEPARATOR,"/",$urlEncoded);
         return $urlEncoded;
